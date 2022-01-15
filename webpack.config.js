b2da9ce302ed9d640819d76036116460340aa3d4
DuJingFei <740677751@qq.com>
2022-01-15 11:14:26 +0800

    1

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const { resolve } = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            // 将css 打包到独立文件中
+            'style-loader', 
+            
+            // css-loader按照CommonJS规范，将样式文件输出到JS中
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.less$/i,
+          use: [
+            // 将css 打包到独立文件中
+            'style-loader', 
+            // css-loader按照CommonJS规范，将样式文件输出到JS中
+            'css-loader',
+  
+            'less-loader',
+          ]
+        }
+      ]
+  }
+}
